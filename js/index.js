34c13347b728e447a6abd546180ca9a005c43586
Sezer-dev <Sezer-dev@users.noreply.github.com>
2020-10-10 10:40:11 +0300

    Added initial project

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const alertBtn = document.getElementById("alertBtn")
+
+alertBtn.addEventListener("click", event => {
+    const alertDiv = document.getElementById("alertDiv");
+    alertDiv.style.display = "none";
+
+})
+
