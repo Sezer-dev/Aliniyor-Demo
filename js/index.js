c43bbbb2cf88b4b143c6e8d3c60389896cd3ba07
Sezer-dev <Sezer-dev@users.noreply.github.com>
2020-10-11 17:50:56 +0300

    Added an eventListener to the close button of the alert element at the top of the page to make a transition when the button pressed

--- js/index.js
+++ js/index.js
@@ -2,7 +2,9 @@ const alertBtn = document.getElementById("alertBtn")
 
 alertBtn.addEventListener("click", event => {
     const alertDiv = document.getElementById("alertDiv");
-    alertDiv.style.display = "none";
-
+    alertDiv.style.transition = "height .4s"
+    alertDiv.style.height = "0"
+    for(let i = 0 ; i < document.querySelectorAll("#alertDiv > * > *").length ; i++ ){
+        document.querySelectorAll("#alertDiv > * > *")[i].style.display = "none";
+    }
 })
-
